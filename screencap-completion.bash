c73f48b9daaa20bf8fbfa454620227d3f2b8ebab
schalox <user1@server2.fake>
2013-12-24 14:35:32 +0200

    Improve the completion, fix the indentation
    - if the current word starts with -, complete opts, else complete filenames
    - don't complete anything after any of the options in $opts_nocomp is given
    - don't complete options after -- is given
    - don't complete anything after a filename is given

--- screencap-completion.bash
+++ screencap-completion.bash
@@ -1,12 +1,29 @@
 _screencap() {
-    local cur prev opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="--help --fps --input --output --filters --blind --mute --threads --"
+  local cur prev opts opts_nocomp
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="--help --fps --input --output --filters --blind --mute --threads --"
+  opts_nocomp="--help|--fps|--input|--output|--filters|--threads"
 
-    [[ "$prev" == "--" ]] && return 0
-    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+  if [[ "$prev" == "--" ]]; then
+    # End of arguments -> complete filenames
+    _filedir
+  elif ((COMP_CWORD > 1)) && [[ -f "$prev" ]]; then
+    # Last argument was a file -> no completion
+    return 0
+  else
+    if [[ $prev =~ $opts_nocomp ]]; then
+      # Last argument was one of $opts_nocomp -> no completion
+      return 0
+    elif [[ "$cur" == -* ]]; then
+      # Current arguments starts with '-' -> complete $opts
+      COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    else
+      # Complete filenames
+      _filedir
+    fi
+  fi
 }
 
 complete -F _screencap screencap
